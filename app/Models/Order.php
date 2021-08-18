8972523387f953097df256e55def6cd217e33b92
NOR ALIAH RAZAK <noraliahrazak@NORs-MacBook-Pro.local>
2021-08-18 17:40:54 +0800

    First commit

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'product_name',
+        'quantity',
+        'price_each',
+        'estimated',
+        'bil_address',
+        'ship_address',
+        'total',
+        'payment_method'       
+    ];
+
+}
---
+++ database/migrations/2021_08_18_071429_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('order_no');
+            $table->string('product_name');
+            $table->string('quantity');
+            $table->string('price_each');
+            $table->string('estimated');
+            $table->string('bil_address');
+            $table->string('ship_address');
+            $table->string('total');
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
